e3091fdd3152d974526a3e92a6045fdc74f16116
imdaano <danisanchezpsc@gmail.com>
2025-01-17 12:52:24 +0100

    add navbar styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import './App.css'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div>
       <Header />
+      <Navbar />
     </div>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <nav>
             <ul>
-                <li><a href="./home">Home</a></li>
+                <li><a href="./home" className='active'>Home</a></li>
                 <li><a href="./users">Users</a></li>
                 <li><a href="./posts">Posts</a></li>
                 <li><a href="/register">Register</a></li>
