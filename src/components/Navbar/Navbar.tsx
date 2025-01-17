e6f8e9764b2e129b8eb03d61fc6bda07d1369f4a
imdaano <danisanchezpsc@gmail.com>
2025-01-17 12:42:07 +0100

    add li to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,16 @@
 import './Navbar.css'
 
 function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <nav>
+            <ul>
+                <li><a href="./home">Home</a></li>
+                <li><a href="./users">Users</a></li>
+                <li><a href="./posts">Posts</a></li>
+                <li><a href="/register">Register</a></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
