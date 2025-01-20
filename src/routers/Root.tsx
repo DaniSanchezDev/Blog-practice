5a4dd1c4bba4e9bc5f511d94ffbeedd1022ab0fc
imdaano <danisanchezpsc@gmail.com>
2025-01-20 10:14:51 +0100

    create routes

---
+++ src/routers/Root.tsx
@@ -0,0 +1,21 @@
+// import Header  from '../src/components/Header'
+
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
+
+
+function Root() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <main>
+
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Root
