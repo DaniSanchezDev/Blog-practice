507071e2ef61f06246773309f345cf27e6af0e40
imdaano <danisanchezpsc@gmail.com>
2025-01-21 10:10:15 +0100

    FEAT add new code to show users

--- src/routers/Users.tsx
+++ src/routers/Users.tsx
@@ -1,28 +1,28 @@
 import { LoaderFunction, useLoaderData } from "react-router-dom"
-import { PostResponse } from "../types/app";
+import { UserResponse } from "../types/app";
 import List from "../components/List";
 
 const loader: LoaderFunction = async function () {
     // creamos la funcion asincrona y le prometemos que la respuesta está esperando por los json de la API
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     // creamos la constante posts para guardar los json de la respuesta. Le ponemos tb el tipado de TS
-    const posts: PostResponse[] = await response.json();
+    const users: UserResponse[] = await response.json();
     // devolvemos todos los posts. Si no el ponemos esta linea cargará TODOS los posts
-    return posts
+    return users
 }
 
 function Users() {
 
-    const posts = useLoaderData() as PostResponse[];
+    const users = useLoaderData() as UserResponse[];
     
   return (
     <>
       <h2>Users</h2>
-      {posts.length === 0 ?
-       <p className="no.items">No posts</p> :
+      {users.length === 0 ?
+       <p className="no.items">No users</p> :
        (
         //Creamos la ruta para los posts que tengan cada id correspondiente
-        <List items={posts.map(post => ({text: post.title, link: `/posts/${post.id}`}))} />
+        <List items={users.map(user => ({text: `${user.name} (${user.email})`, link: `/users/${user.id}`}))} />
        )}
 
     </>
